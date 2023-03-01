25cc4b5065b5ae239ef52ebe8933d6ccfba12026
Kleiton Utida <kleiton.utida@live.com>
2023-02-28 23:50:05 -0300

    Add negociacoes and list negociacoes

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,5 +1,13 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
-  private negociacoes: Array<Negociacao> = []
+  private negociacoes: Array<Negociacao> = [];
+
+  adiciona(negociacao: Negociacao) {
+    this.negociacoes.push(negociacao);
+  }
+
+  lista(): ReadonlyArray<Negociacao> {
+    return this.negociacoes;
+  }
 }
