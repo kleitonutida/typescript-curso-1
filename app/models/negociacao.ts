722e67b5ad7bc32d1f3b6250a667aca8f472d5e0
Kleiton Utida <kleiton.utida@live.com>
2023-03-01 23:50:02 -0300

    Simplifying the code

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,21 +1,9 @@
 export class Negociacao {
   constructor(
-    private _data: Date,
-    private _quantidade: number,
-    private _valor: number
+    private readonly _data: Date,
+    private readonly _quantidade: number,
+    private readonly _valor: number
   ) {}
-
-  get data(): Date {
-    return this._data;
-  }
-
-  get quantidade(): number {
-    return this._quantidade;
-  }
-
-  get valor(): number {
-    return this._valor;
-  }
 
   get volume(): number {
     return this._quantidade * this.valor;
