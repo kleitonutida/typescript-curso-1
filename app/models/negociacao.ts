d2c7b8108c69fe0a2577d5abf9176b51db128825
Kleiton Utida <kleiton.utida@live.com>
2023-03-02 00:08:04 -0300

    Defensive programming

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,11 +1,15 @@
 export class Negociacao {
   constructor(
-    private readonly _data: Date,
-    private readonly _quantidade: number,
-    private readonly _valor: number
+    public _data: Date,
+    private readonly quantidade: number,
+    private readonly valor: number
   ) {}
 
   get volume(): number {
-    return this._quantidade * this._valor;
+    return this.quantidade * this.valor;
+  }
+
+  get data() {
+    return new Date(this._data.getTime())
   }
 }
