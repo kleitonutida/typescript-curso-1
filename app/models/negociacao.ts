57e680875a803482e0a47ec2075fd0fd0565df2c
Kleiton Utida <kleiton.utida@live.com>
2023-03-03 23:28:55 -0300

    Create list

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,8 +1,8 @@
 export class Negociacao {
   constructor(
-    public _data: Date,
-    private readonly quantidade: number,
-    private readonly valor: number
+    private _data: Date,
+    public readonly quantidade: number,
+    public readonly valor: number
   ) {}
 
   get volume(): number {
