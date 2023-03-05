ffcab1dd133a1a8e93d657d017376077bd144f50
Kleiton Utida <kleiton.utida@live.com>
2023-03-04 23:57:47 -0300

    Using generics in inheritance

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -1,7 +1,7 @@
 import { Negociacoes } from "../models/negociacoes.js";
 import { View } from "./view.js";
 
-export class NegociacoesView extends View {
+export class NegociacoesView extends View<Negociacoes> {
   template(model: Negociacoes): string {
     return `
     <table class="table table-hover table-bordered">
@@ -28,10 +28,5 @@ export class NegociacoesView extends View {
       </tbody>
     </table>
     `;
-  }
-
-  update(model: Negociacoes): void {
-    const template = this.template(model);
-    this.elemento.innerHTML = template;
   }
 }
